73d3745725af5295b6567389e12c9999bfc7a180
Steven Sandoval <s.sandoval2018@gmail.com>
2022-05-09 20:06:59 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using BankApplication;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Globalization;
+
+namespace BankApplication
+{
+ 
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Wallet wallet = new Wallet("Steven", 21, 100.0, 25.50);
+            Console.WriteLine($"Funds: {wallet.WalletMoney}");
+            Console.WriteLine($"Price: {wallet.Prices}");
+
+            if(wallet.WalletMoney > wallet.Prices)
+            {
+                Console.WriteLine($"Purchase has went through" );
+                double remaining = wallet.WalletMoney - wallet.Prices;
+                Console.WriteLine($"Remaining: {remaining}");
+            }
+            
+            
+            
+
+        
+        }
+
+
+    }
+}
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        public static bool IsValidEmail(string email)
+//        {
+//            if (string.IsNullOrWhiteSpace(email))
+//                return false;
+
+//            try
+//            {
+//                email = Regex.Replace(email, @"(@)(.+)$", DomainMapper,
+//                                      RegexOptions.None, TimeSpan.FromMilliseconds(200));
+
+//                string DomainMapper(Match match)
+//                {
+//                    var idn = new IdnMapping();
+
+//                    string domainName = idn.GetAscii(match.Groups[2].Value);
+
+//                    return match.Groups[1].Value + domainName;
+//                }
+//            }
+//            catch (RegexMatchTimeoutException e)
+//            {
+//                return false;
+//            }
+//            catch (ArgumentException e)
+//            {
+//                return false;
+//            }
+
+//            try
+//            {
+//                return Regex.IsMatch(email,
+//                    @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+//                    RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+//            }
+//            catch (RegexMatchTimeoutException)
+//            {
+//                return false;
+//            }
+//        }
+//    }
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//            private static string[] CapitalizeNames(string[] names)
+//            {
+//                string[] capitalizedNames = new string[names.Length];
+//                for (int i = 0; i < names.Length; i++)
+//                {
+//                    char[] characterArray = names[i].ToCharArray();
+//                    for (int j = 0; j < characterArray.Length; j++)
+//                    {
+//                        if (j == 0)
+//                        {
+//                            string firstLetter = characterArray[0].ToString().ToUpper();
+//                            characterArray[0] = Convert.ToChar(firstLetter);
+//                        }
+//                        else
+//                        {
+//                            string followingLetter = characterArray[j].ToString().ToLower();
+//                            characterArray[j] = followingLetter.ToCharArray()[0];
+//                        }
+//                    }
+//                    capitalizedNames[i] = string.Join("", characterArray);
+//                }
+
+//                return capitalizedNames;
+//            }
+//        }
+//    }
+
+
+//}
---
+++ Wallet.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApplication
+{
+    internal class Wallet : Person
+    {
+        private double _walletFunds;
+        private double _price;
+        public Wallet(string name, int age, double walletMoney, double prices) : base(name, age)
+        {
+            name = "Steven ";
+            age = 21;
+            _walletFunds = walletMoney;
+            _price = prices;
+
+
+        }
+        public void PrintMessage()
+        {
+            double wallet = WalletFunds();
+            double price = Price();
+            IsBalance(wallet, price);
+
+        }
+        public static double WalletFunds()
+        {
+            double wallet = 100.0;
+            return wallet;
+        }
+        public static double Price()
+        {
+            double price = 25.50;
+            return price;
+        }
+        public static bool IsBalance(double wallet, double price)
+        {
+            bool expression = false;
+            if (wallet > price)
+            {
+                expression = true;
+                wallet -= price;
+
+                Console.WriteLine(wallet);
+            }
+            else if (wallet < price)
+            {
+                return false;
+            }
+
+            return expression;
+        }
+        public double WalletMoney { get { return _walletFunds; } }
+        public double Prices { get { return _price; } }
+    }
+}
